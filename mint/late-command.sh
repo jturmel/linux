355853f971266d4c0c27cd8da089260f2912b7b8
Josh Turmel <jturmel@gmail.com>
2025-08-29 13:20:45 -0500

    feat: Install JetBrains Toolbox to user's local share

--- mint/late-command.sh
+++ mint/late-command.sh
@@ -3,3 +3,10 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
 apt-get install -y google-chrome-stable
+
+# Install JetBrains Toolbox
+apt-get install -y libfuse2
+wget -O /tmp/jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+mkdir -p $HOME/.local/.share/JetBrains/Toolbox
+tar -xzf /tmp/jetbrains-toolbox.tar.gz -C $HOME/.local/.share/JetBrains/Toolbox --strip-components=1
+$HOME/.local/.share/JetBrains/Toolbox/jetbrains-toolbox
