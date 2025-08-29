2ce7691647be40ef1dead7df101512126c73bdc3
Josh Turmel <jturmel@gmail.com>
2025-08-29 14:00:30 -0500

    feat: Add GitHub CLI installation to late-command

--- mint/late-command.sh
+++ mint/late-command.sh
@@ -10,3 +10,9 @@ wget -O /tmp/jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/produ
 mkdir -p $HOME/.local/.share/JetBrains/Toolbox
 tar -xzf /tmp/jetbrains-toolbox.tar.gz -C $HOME/.local/.share/JetBrains/Toolbox --strip-components=1
 $HOME/.local/.share/JetBrains/Toolbox/jetbrains-toolbox
+
+# Install GitHub CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt-get update
+apt-get install -y gh
