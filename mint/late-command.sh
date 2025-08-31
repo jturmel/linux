f9f717ffc97ca4ec0dac3829a7a83351d9bdafaa
Josh Turmel <jturmel@gmail.com>
2025-08-31 13:02:16 -0500

    Add git installation

--- mint/late-command.sh
+++ mint/late-command.sh
@@ -1,4 +1,9 @@
+
 #!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git-all
+
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
