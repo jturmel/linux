bea65da6c8d62153206dda4286cb741c011363a2
Josh Turmel <jturmel@gmail.com>
2025-09-01 17:57:18 -0500

    Fix error in if file exists check for theme

--- mint/config/theme.sh
+++ mint/config/theme.sh
@@ -3,9 +3,11 @@
 mkdir -p $HOME/.themes
 wget https://github.com/dracula/gtk/archive/master.zip -O /tmp/dracula-gtk.zip
 unzip -o /tmp/dracula-gtk.zip -d $HOME/.themes/
-if [-e $HOME/.themes/Dracula ]; then
+
+if [ -e "$HOME/.themes/Dracula" ]; then
   rm -rf $HOME/.themes/Dracula
 fi
+
 mv $HOME/.themes/gtk-master $HOME/.themes/Dracula
 gsettings set org.cinnamon.desktop.interface gtk-theme "Dracula"
 gsettings set org.cinnamon.desktop.wm.preferences theme "Dracula"
