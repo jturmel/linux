c00c00021271fb4e9755b74c5d196f207f7bf946
Josh Turmel <jturmel@gmail.com>
2025-09-01 16:11:40 -0500

    Cleanup Firefox and Thunderbird

--- mint/cleanup.sh
+++ mint/cleanup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo apt -y purge firefox
+sudo apt purge -y firefox
+sudo apt purge -y thunderbird
+
+rm -r $HOME/.mozilla
+rm -r $HOME/.cache/mozilla
+
+sudo apt autoremove --purge
 
