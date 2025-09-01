1564ae096a5d0770287c8276551ec43c3d10296e
Josh Turmel <jturmel@gmail.com>
2025-09-01 17:52:57 -0500

    If Dracula theme exists already, clear it out

--- mint/config/theme.sh
+++ mint/config/theme.sh
@@ -3,6 +3,9 @@
 mkdir -p $HOME/.themes
 wget https://github.com/dracula/gtk/archive/master.zip -O /tmp/dracula-gtk.zip
 unzip -o /tmp/dracula-gtk.zip -d $HOME/.themes/
+if [-e $HOME/.themes/Dracula ]; then
+  rm -rf $HOME/.themes/Dracula
+fi
 mv $HOME/.themes/gtk-master $HOME/.themes/Dracula
 gsettings set org.cinnamon.desktop.interface gtk-theme "Dracula"
 gsettings set org.cinnamon.desktop.wm.preferences theme "Dracula"
