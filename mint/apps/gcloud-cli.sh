2470ce6dd2caf076d7d1523234e5e2b65ec18e15
Josh Turmel <jturmel@gmail.com>
2025-09-01 17:48:39 -0500

    Add gcloud cmd completions and location to profile

--- mint/apps/gcloud-cli.sh
+++ mint/apps/gcloud-cli.sh
@@ -5,3 +5,8 @@ tar -xf google-cloud-cli-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --quiet
 rm google-cloud-cli-linux-x86_64.tar.gz
 
+echo "### google-cloud-sdk start ###" >> $HOME/.zshrc
+echo 'source $HOME/google-cloud-sdk/completion.zsh.inc' >> $HOME/.zshrc
+echo 'source $HOME/google-cloud-sdk/path.zsh.inc' >> $HOME/.zshrc
+echo "### google-cloud-sdk end ###" >> $HOME/.zshrc
+
