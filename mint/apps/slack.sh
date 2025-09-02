77fda982ce6a820414201842a3ac4b9e19181330
Josh Turmel <jturmel@gmail.com>
2025-09-01 19:09:51 -0500

    Update Slack install

--- mint/apps/slack.sh
+++ mint/apps/slack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#
-wget -O /tmp/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.36.137-amd64.deb"
-sudo apt install -y /tmp/slack.deb
-rm /tmp/slack.deb
+
+sudo apt install -y slack
+# wget -O /tmp/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.36.137-amd64.deb"
+# sudo apt install -y /tmp/slack.deb
+# rm /tmp/slack.deb
 
