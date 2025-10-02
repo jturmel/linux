6c87b49d87532c8da8a1ea6b4eb664df47e3b32f
Josh Turmel <jturmel@gmail.com>
2025-10-02 10:30:07 -0500

    Update Slack installer

--- ubuntu/apps/slack.sh
+++ ubuntu/apps/slack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y slack
-# wget -O /tmp/slack.deb "https://downloads.slack-edge.com/linux_releases/slack-desktop-4.36.137-amd64.deb"
-# sudo apt install -y /tmp/slack.deb
-# rm /tmp/slack.deb
-
+# TODO Dynamically determine the most up-to-date file to grab
+#   as Slack removes old versions when they release a new one
+wget -O /tmp/slack.deb "https://downloads.slack-edge.com/desktop-releases/linux/x64/4.46.99/slack-desktop-4.46.99-amd64.deb"
+sudo apt install -y /tmp/slack.deb
+rm /tmp/slack.deb
