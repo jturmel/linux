2e863440a5f7978229742ba61d85a693354eb749
Josh Turmel <jturmel@gmail.com>
2025-10-03 15:16:57 -0500

    Make global .gitignore idempotent

--- ubuntu/config/git.sh
+++ ubuntu/config/git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -e "$HOME/.gitignore" ]; then
-  return 0
-else
-  touch "$HOME"/.gitignore
-  curl -fsSL https://jturmel.github.io/dotfiles/.gitignore -o "$HOME/.gitignore"
+  rm "$HOME/.gitignore"
 fi
+
+touch "$HOME"/.gitignore
+curl -fsSL https://jturmel.github.io/dotfiles/.gitignore -o "$HOME/.gitignore"
 
 git config --global core.excludesFile "$HOME/.gitignore"
