4a3418fc01a1cdf0b8d8478b17bf84a629bb67a2
Josh Turmel <jturmel@gmail.com>
2025-10-13 14:23:16 -0500

    Update gcloud-cli and nodejs

--- ubuntu/apps/gcloud-cli.sh
+++ ubuntu/apps/gcloud-cli.sh
@@ -13,8 +13,10 @@ if ! grep -qF "$search_string" "$file_path"; then
   echo "Installing .zshrc includes."
   echo "### google-cloud-sdk start ###" >>$HOME/.zshrc
   echo 'source $HOME/google-cloud-sdk/completion.zsh.inc' >>$HOME/.zshrc
-  echo 'source $HOME/google-cloud-sdk/path.zsh.inc' >>$HOME/.zshrc
   echo "### google-cloud-sdk end ###" >>$HOME/.zshrc
 else
   echo "Skipping .zshrc includes, already installed."
 fi
+
+# Setup link in /usr/bin so that gcloud is accessible for all tools that may need to call it
+sudo ln -s ~/google-cloud-sdk/bin/gcloud /usr/bin/gcloud
--- ubuntu/apps/nodejs.sh
+++ ubuntu/apps/nodejs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 22
-
