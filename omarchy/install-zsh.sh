579e621b2718d26048f3895fbca2f007c76cd27c
Josh Turmel <jturmel@gmail.com>
2025-11-17 00:08:47 -0600

    Add powerlevel10k theme to zsh

--- omarchy/install-zsh.sh
+++ omarchy/install-zsh.sh
@@ -6,3 +6,6 @@ chsh -s /usr/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
