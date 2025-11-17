aa3d07999e1b9970605ed1eb96ece3334709f04e
Josh Turmel <jturmel@gmail.com>
2025-11-16 21:20:15 -0600

    Add ack to install

--- omarchy/install.sh
+++ omarchy/install.sh
@@ -3,6 +3,7 @@
 WORK_DIR=$(dirname "$0")
 
 # Install additional packages
+. $WORK_DIR/install-ack.sh
 . $WORK_DIR/install-google-chrome.sh
 . $WORK_DIR/install-stow.sh
 . $WORK_DIR/install-jetbrains-toolbox.sh
