573a6b3e0fdd91275aa9b3c4327cf1983a96d790
Josh Turmel <jturmel@gmail.com>
2025-11-18 16:58:00 -0600

    Add ohmyposh and google-cloud-cli-* tools

---
+++ omarchy/install-google-cloud.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+yay -S --noconfirm google-cloud-cli
+yay -S --noconfirm google-cloud-cli-bq
+yay -S --noconfirm google-cloud-gsutil
+
--- omarchy/install.sh
+++ omarchy/install.sh
@@ -7,8 +7,10 @@ WORK_DIR=$(dirname "$0")
 . $WORK_DIR/install-dracula-theme.sh
 . $WORK_DIR/install-gemini-cli.sh
 . $WORK_DIR/install-google-chrome.sh
+. $WORK_DIR/install-google-cloud.sh
 . $WORK_DIR/install-jetbrains-toolbox.sh
 . $WORK_DIR/install-kitty.sh
+. $WORK_DIR/install-oh-my-posh.sh
 . $WORK_DIR/install-rsync.sh
 . $WORK_DIR/install-screamingfrogseospider.sh
 . $WORK_DIR/install-slack.sh
