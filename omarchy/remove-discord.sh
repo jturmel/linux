6b49d7aaa4a2c21cafd08c8c92e23e86c0874a4c
Josh Turmel <jturmel@gmail.com>
2025-11-21 13:05:00 -0600

    Remove discord web app

---
+++ omarchy/remove-discord.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+omarchy-webapp-remove Discord
+
--- omarchy/remove.sh
+++ omarchy/remove.sh
@@ -5,4 +5,7 @@ WORK_DIR=$(dirname "$0")
 # Remove unwanted packages
 . $WORK_DIR/remove-omarchy-chromium.sh
 . $WORK_DIR/remove-alacritty.sh
+. $WORK_DIR/remove-signal.sh
+. $WORK_DIR/remove-spotify.sh
+. $WORK_DIR/remove-discord.sh
 
