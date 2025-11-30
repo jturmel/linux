40762c12464fe437190a017522c2aec660a6617b
Josh Turmel <jturmel@gmail.com>
2025-11-29 22:49:11 -0600

    Remove installation of powerlevel10k

--- omarchy/install-zsh.sh
+++ omarchy/install-zsh.sh
@@ -6,6 +6,3 @@ chsh -s /usr/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-yay -S --noconfirm zsh-theme-powerlevel10k-git
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
