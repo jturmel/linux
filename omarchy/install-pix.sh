69ad8a9b513dc4a9611a82b01802776baff5b20f
Josh Turmel <jturmel@gmail.com>
2025-12-18 11:13:11 -0600

    Add Pix to installed packages

---
+++ omarchy/install-pix.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm --needed pix
+
--- omarchy/install.sh
+++ omarchy/install.sh
@@ -13,6 +13,7 @@ WORK_DIR=$(dirname "$0")
 . $WORK_DIR/install-jetbrains-toolbox.sh
 . $WORK_DIR/install-kitty.sh
 . $WORK_DIR/install-oh-my-posh.sh
+. $WORK_DIR/install-pix.sh
 . $WORK_DIR/install-rsync.sh
 . $WORK_DIR/install-screamingfrogseospider.sh
 . $WORK_DIR/install-slack.sh
