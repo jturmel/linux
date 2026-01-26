6f3fcae39919a66b3faee60cc59dc6c3568eabbf
Josh Turmel <jturmel@gmail.com>
2026-01-26 10:46:25 -0600

    Remove the removal of Basecamp

--- omarchy/remove-basecamp.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-omarchy-webapp-remove Basecamp
-
--- omarchy/remove.sh
+++ omarchy/remove.sh
@@ -4,7 +4,6 @@ WORK_DIR=$(dirname "$0")
 
 # Remove unwanted packages
 . $WORK_DIR/remove-alacritty.sh
-. $WORK_DIR/remove-basecamp.sh
 . $WORK_DIR/remove-discord.sh
 . $WORK_DIR/remove-hey.sh
 . $WORK_DIR/remove-omarchy-chromium.sh
