4b95550e70a659e6c86d8912bdc40845d27a081a
Josh Turmel <jturmel@gmail.com>
2026-03-05 01:00:01 -0600

    Add docker containers

--- omarchy/config-docker-containers.sh
+++ omarchy/config-docker-containers.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -d -p 45484:45484 --restart unless-stopped --name blackd ceeveeya/blackd:latest
-docker run yuzutech/kroki --restart unless-stopped --name kroki
+docker run -d -p 7105:8000 --restart unless-stopped --name kroki yuzutech/kroki:latest
+docker run -d -p 11235:11235 --restart unless-stopped --shm-size=1g --name crawl4ai unclecode/crawl4ai:latest
+
